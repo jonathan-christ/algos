616bc799cd3274d85ee350ab7bf0df3020d89f11
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-09-18 22:38:32 +0800

    feat(bubble_sort): added bubble sort

---
+++ bubble_sort/bubble_sort.c
@@ -0,0 +1,36 @@
+#include "bubble_sort.h"
+#include "../utils.c"
+
+int main()
+{
+  cellOutput = bubbleSort(data, MAX_ARR_LEN);
+  printCellArr(cellOutput, MAX_ARR_LEN);
+  free(cellOutput);
+}
+
+Cell *bubbleSort(Cell arr[], int size)
+{
+  int memSize = size * sizeof(Cell);
+  Cell temp, *output = (Cell *)malloc(memSize);
+  memcpy(output, arr, memSize);
+
+  for (int j = 0; j < size; j++)
+  {
+    for (int i = 1; i < size; i++)
+    {
+      if (output[i - 1].value > output[i].value)
+      {
+        swap(&output[i-1], &output[i]);
+      }
+    }
+  }
+
+  return output;
+}
+
+void swap(Cell *A, Cell *B)
+{ 
+  Cell temp = *A;
+  *A = *B;
+  *B = temp;
+}
---
+++ bubble_sort/bubble_sort.h
@@ -0,0 +1,8 @@
+#ifndef BUBBLE_SORT_H_
+#define BUBBLE_SORT_H_
+#include "../utils.h"
+
+void swap(Cell *, Cell *);
+Cell *bubbleSort(Cell [], int);
+
+#endif
