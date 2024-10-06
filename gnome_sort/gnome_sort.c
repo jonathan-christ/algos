3090a26b0f4b87dd5a374dc0175f31b2e7ddfa6f
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-10-06 17:06:20 +0800

    feat(gnome_sort): added gnome sort

---
+++ gnome_sort/gnome_sort.c
@@ -0,0 +1,38 @@
+#include "gnome_sort.h"
+#include "../utils.c"
+
+int main()
+{
+  cellOutput = (Cell *)malloc(sizeof(Cell) * MAX_ARR_LEN);
+  if (cellOutput != NULL)
+  {
+    memcpy(cellOutput, data, sizeof(Cell) * MAX_ARR_LEN);
+    gnomeSort(cellOutput, MAX_ARR_LEN);
+    printCellArr(cellOutput, MAX_ARR_LEN);
+    free(cellOutput);
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+void gnomeSort(Cell arr[], int size)
+{
+  int i = 1;
+  Cell temp;
+  while (i < size)
+  {
+    if (i!=0 && arr[i].value < arr[i - 1].value)
+    {
+      temp = arr[i];
+      arr[i] = arr[i - 1];
+      arr[i - 1] = temp;
+      i--;
+    }
+    else
+    {
+      i++;
+    }
+  }
+}
---
+++ gnome_sort/gnome_sort.h
@@ -0,0 +1,7 @@
+#ifndef GNOME_SORT_H_
+#define GNOME_SORT_H_
+#include "../utils.h"
+
+void gnomeSort(Cell[], int);
+
+#endif
